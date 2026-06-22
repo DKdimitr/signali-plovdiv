786a2a1972dab61dab3018d622bf1842b4d75853
DKdimitr <dkbusiness901@gmail.com>
2026-06-22 15:07:46 +0300

    Add latitude and longitude to signals selection

--- api/get-signals.js
+++ api/get-signals.js
@@ -22,7 +22,7 @@ export default async function handler(request, response) {
     // Скриваме трите имена, телефона и имейла на гражданите!
     const { data, error } = await supabase
       .from('signals')
-      .select('id, created_at, corrected_text, location, assigned_institution, priority, status, image_url')
+      .select('id, created_at, corrected_text, location, assigned_institution, priority, status, image_url, latitude, longitude')
       .order('created_at', { ascending: false });
 
     if (error) throw error;
